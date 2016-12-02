6ad12a6f6985a276a453f341c8e6b6518d7afe24
Sergey Voroshilov <voroshilov.grk@yandex.ru>
2016-12-02 17:48:21 +0300

    Добавлена функция создания коллекции сетей в БД и еще кое-что

--- nnDB.pas
+++ nnDB.pas
@@ -5,7 +5,7 @@ unit nnDB;
 interface
 
 uses
-	Classes, IBConnection, nnLog, nnTypes;
+	Classes, IBConnection, sqldb, nnLog, nnTypes;
 
 type
 	PDataBase = ^TDataBase;
@@ -19,6 +19,8 @@ type
 		procedure AfterConnect(Sender: TObject);
 		procedure BeforeDisconnect(Sender: TObject);
 		procedure AfterDisconnect(Sender: TObject);
+		function InitTransaction: TSQLTransaction;
+		function InitQuery(const aTransaction: TSQLTransaction): TSQLQuery;
 	public
 		constructor Create;
 		destructor Destroy; override;
@@ -27,6 +29,9 @@ type
 		procedure SetConnectionParams(const CharSet, DatabaseName, 
 			HostName, UserName, Password: String);
 		function Connect: Boolean;
+		function TablesExist: Boolean;
+		function CreateCollection(const aSymbol: String;  const aInterval: TInterval;
+			const aLastBar: TDateTime; const aParams: TMLPParams): Integer;
 	end;
 
 implementation
@@ -61,6 +66,19 @@ procedure TDataBase.AfterDisconnect(Sender: TObject);
 begin
 	AddToLog('[OK]', Empty);
 	AddToLog('Подключение к ' + fConnection.DatabaseName + ' разорвано', Info);
+end;
+
+function TDataBase.InitTransaction: TSQLTransaction;
+begin
+	InitTransaction := TSQLTransaction.Create(nil);
+	InitTransaction.Database := fConnection;
+end;
+
+function TDataBase.InitQuery(const aTransaction: TSQLTransaction): TSQLQuery;
+begin
+	InitQuery := TSQLQuery.Create(nil);
+	InitQuery.Database := fConnection;
+	InitQuery.Transaction := aTransaction;
 end;
 
 constructor TDataBase.Create;
@@ -115,29 +133,95 @@ begin
 	try
 		fConnection.Open;
 	except
-		on E: {Exception}EIBDatabaseError Do
-			// case E.GDSErrorCode of
-			// 	335544472 : begin
-			// 					AddToLog('Ошибка подключения:', Error); 
-			// 					AddToLog('   Не верный логин или пароль', Error);
-			// 				end;
-			// 	335544721 : begin
-			// 					AddToLog('Ошибка подключения:', Error);
-			// 					AddToLog('   Не удалось подключиться к ' 
-			// 						+ fConnection.HostName, Error);
-			// 				end;
-			// 	335544344 : begin
-			// 					AddToLog('Ошибка подключения:', Error);
-			// 					AddToLog('   База ' + fConnection.DatabaseName
-			// 						+ ' не найдена', Error);
-			// 				end;
-			// 	else
-			// 		AddToLog(IntToStr(E.GDSErrorCode), Error);
-			// end;
-				AddToLog(E.Message, Error);
+		on E: EIBDatabaseError Do
+			AddToLog(E.Message, Error);
 	end;
-	
+
 	Connect := fConnection.Connected;
+end;
+
+function TDataBase.TablesExist: Boolean;
+var
+	Transaction: TSQLTransaction;
+	Query: TSQLQuery;
+begin
+	if not fConnection.Connected then
+	begin
+		AddToLog('Подключение к базе данных не установлено', Error);
+		Exit;
+	end;
+
+	Transaction := InitTransaction;
+	Query := InitQuery(Transaction);
+
+	try
+		Query.SQL.Add('select RDB$RELATION_NAME from RDB$RELATIONS where (RDB$SYSTEM_FLAG = 0) AND (RDB$RELATION_TYPE = 0) order by RDB$RELATION_NAME');
+		Transaction.StartTransaction;
+		Query.Open;
+		AddToLog(IntToStr(Query.RowsAffected));
+		Query.Close;
+		Transaction.Commit;
+	except
+		on E: Exception Do
+		begin
+			AddToLog(E.Message, Error);
+			Transaction.Rollback;
+		end;
+	end;
+
+	FreeAndNil(Query);
+	FreeAndNil(Transaction);
+end;
+
+function TDataBase.CreateCollection(const aSymbol: String; const aInterval: TInterval;
+	const aLastBar: TDateTime; const aParams: TMLPParams): Integer;
+var
+	Transaction: TSQLTransaction;
+	Query: TSQLQuery;
+	Interval: Integer;
+begin
+	CreateCollection := 0;
+
+	if not fConnection.Connected then
+	begin
+		AddToLog('Подключение к базе данных не установлено', Error);
+		Exit;
+	end;
+
+	Transaction := InitTransaction;
+	Query := InitQuery(Transaction);
+
+	try
+		Transaction.StartTransaction;
+		with Query do
+		begin
+			SQL.Add('EXECUTE PROCEDURE PROC_CREATE_COLLECTION(:symbol, :interval, :last_bar, :step_count, :train_count_min, :train_count_max, :inner_count_min, :inner_count_max, :hide_count_min, :hide_count_max, :class_count_min, :class_count_max)');
+			Interval := Ord(aInterval) + 1;
+
+			ParamByName('symbol').AsString := aSymbol;
+			ParamByName('interval').AsInteger := Interval;
+			ParamByName('last_bar').AsDateTime := aLastBar;
+			ParamByName('step_count').AsInteger := aParams.StepCount;
+			ParamByName('train_count_min').AsInteger := aParams.TrainCountRange.min;
+			ParamByName('train_count_max').AsInteger := aParams.TrainCountRange.max;
+			ParamByName('inner_count_min').AsInteger := aParams.InnerCountRange.min;
+			ParamByName('inner_count_max').AsInteger := aParams.InnerCountRange.max;
+			ParamByName('hide_count_min').AsInteger  := aParams.HideCountRange.min;
+			ParamByName('hide_count_max').AsInteger  := aParams.HideCountRange.max;
+			ParamByName('class_count_min').AsInteger := aParams.ClassCountRange.min;
+			ParamByName('class_count_max').AsInteger := aParams.ClassCountRange.max;
+			Open;
+			CreateCollection := FieldByName('id').AsInteger;
+		end;
+		Transaction.Commit;
+		AddToLog('В базу данных добавлен набор сетей ' + IntToStr(CreateCollection));
+	except
+		on E: Exception Do
+		begin
+			AddToLog(E.Message, Error);
+			Transaction.Rollback;
+		end;
+	end;
 end;
 
 end.
